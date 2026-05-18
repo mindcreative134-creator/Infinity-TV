050cad380d8b319ccfc1e73c333b88ed56a9a2f9
mindcreative134-creator <mindcreative134@gmail.com>
2026-05-18 18:38:23 +0530

    Fix: Remove build directory redirection to resolve IDE sync phased action crash

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,18 +5,10 @@ allprojects {
     }
 }
 
-val newBuildDir = layout.buildDirectory.dir("../../build")
-layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir = newBuildDir.map { it.dir(project.name) }
-    layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
-    delete(layout.buildDirectory)
+    delete(rootProject.layout.buildDirectory)
 }
